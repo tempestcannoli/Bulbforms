dd4ae2b7e4baf1936a2e72700879eb3da6256b35
tempestcannoli <92439001+tempestcannoli@users.noreply.github.com>
2023-02-11 21:32:05 +0200

    Add project files.

---
+++ Bulbforms/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.UBulb1 = New Lightbulb.UBulb()
+        Me.UBulb2 = New Lightbulb.UBulb()
+        Me.btnOff = New System.Windows.Forms.Button()
+        Me.btnOn = New System.Windows.Forms.Button()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.btnTest2 = New System.Windows.Forms.Button()
+        Me.UCtrlButton1 = New onoffbutton.UCtrlButton()
+        Me.SuspendLayout()
+        '
+        'UBulb1
+        '
+        Me.UBulb1.Location = New System.Drawing.Point(0, 0)
+        Me.UBulb1.Name = "UBulb1"
+        Me.UBulb1.Size = New System.Drawing.Size(800, 450)
+        Me.UBulb1.TabIndex = 0
+        '
+        'UBulb2
+        '
+        Me.UBulb2.Location = New System.Drawing.Point(32, 20)
+        Me.UBulb2.Name = "UBulb2"
+        Me.UBulb2.Size = New System.Drawing.Size(506, 519)
+        Me.UBulb2.TabIndex = 1
+        '
+        'btnOff
+        '
+        Me.btnOff.Location = New System.Drawing.Point(607, 34)
+        Me.btnOff.Name = "btnOff"
+        Me.btnOff.Size = New System.Drawing.Size(75, 23)
+        Me.btnOff.TabIndex = 2
+        Me.btnOff.Text = "Off"
+        Me.btnOff.UseVisualStyleBackColor = True
+        '
+        'btnOn
+        '
+        Me.btnOn.Location = New System.Drawing.Point(607, 92)
+        Me.btnOn.Name = "btnOn"
+        Me.btnOn.Size = New System.Drawing.Size(75, 23)
+        Me.btnOn.TabIndex = 3
+        Me.btnOn.Text = "On"
+        Me.btnOn.UseVisualStyleBackColor = True
+        '
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(805, 484)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnTest.TabIndex = 5
+        Me.btnTest.Text = "On"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'btnTest2
+        '
+        Me.btnTest2.Location = New System.Drawing.Point(805, 438)
+        Me.btnTest2.Name = "btnTest2"
+        Me.btnTest2.Size = New System.Drawing.Size(75, 23)
+        Me.btnTest2.TabIndex = 6
+        Me.btnTest2.Text = "Off"
+        Me.btnTest2.UseVisualStyleBackColor = True
+        '
+        'UCtrlButton1
+        '
+        Me.UCtrlButton1.Location = New System.Drawing.Point(607, 181)
+        Me.UCtrlButton1.Name = "UCtrlButton1"
+        Me.UCtrlButton1.Size = New System.Drawing.Size(273, 196)
+        Me.UCtrlButton1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(922, 551)
+        Me.Controls.Add(Me.btnTest2)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.UCtrlButton1)
+        Me.Controls.Add(Me.btnOn)
+        Me.Controls.Add(Me.btnOff)
+        Me.Controls.Add(Me.UBulb2)
+        Me.Controls.Add(Me.UBulb1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents UBulb1 As Lightbulb.UBulb
+    Friend WithEvents UBulb2 As Lightbulb.UBulb
+    Friend WithEvents btnOff As Button
+    Friend WithEvents btnOn As Button
+    Friend WithEvents UCtrlButton1 As onoffbutton.UCtrlButton
+    Friend WithEvents btnTest As Button
+    Friend WithEvents btnTest2 As Button
+End Class
---
+++ Bulbforms/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnOff.Click
+        UBulb2.setMood("off")
+    End Sub
+
+    Private Sub btnOn_Click(sender As Object, e As EventArgs) Handles btnOn.Click
+        UBulb2.setMood("on")
+    End Sub
+
+    Private Sub btnTest2_Click(sender As Object, e As EventArgs) Handles btnTest2.Click
+        UCtrlButton1.setMood("off")
+    End Sub
+
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        UCtrlButton1.setMood("on")
+    End Sub
+End Class
